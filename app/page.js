bf25e5ee77e1efe4579a05d0e2e875c556ff0bee
Marcos Sanabria <sanabriam007@gmail.com>
2024-07-31 17:26:51 -0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
+import { collection, query, getDocs } from "firebase/firestore";
+
+export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+      {inventory.map((item) => (
+        <div key={item.name}>
+          {item.name}
+          {item.count}
+        </div>
+      ))}
+    </Box>
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYX6bsDMxxd1W3im4w84HPqlu-0fpUARA",
+  authDomain: "inventory-management-ff8d6.firebaseapp.com",
+  projectId: "inventory-management-ff8d6",
+  storageBucket: "inventory-management-ff8d6.appspot.com",
+  messagingSenderId: "1031743163746",
+  appId: "1:1031743163746:web:4f9e1858521f6a9f53703b",
+  measurementId: "G-5J3TS3PXTL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export { firestore };
