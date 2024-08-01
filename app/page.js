20f1a1d2d38d80fb7d0deb192b086621aafb45d6
Marcos Sanabria <sanabriam007@gmail.com>
2024-08-01 10:03:50 -0500

    added dynamic item list as well as buttons to add and delete

--- app/page.js
+++ app/page.js
@@ -144,10 +144,47 @@ export default function Home() {
             Inventory Items
           </Typography>
         </Box>
+
+        <Stack width="800px" height="300px" spacing={2} overflow="auto">
+          {inventory.map(({ name, quantity }) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              bgcolor="#f0f0f0"
+              padding={5}
+            >
+              <Typography variant="h3" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {quantity}
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    addItem(name);
+                  }}
+                >
+                  Add
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    removeItem(name);
+                  }}
+                >
+                  Remove
+                </Button>
+              </Stack>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-      <Stack wdith="100px" height="300px" spacing={2} overflow="auto">
-        
-      </Stack>
     </Box>
   );
 }
